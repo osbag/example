2876ce922ddf88a13a3f36b6b95ea471f0caf7a2
Hardik Mehta <hardik.mehta.external@zeppelin.com>
2018-04-23 13:53:28 +0200

    Initial commit

---
+++ src/main/java/de/osbag/example/service/UserService.java
@@ -0,0 +1,25 @@
+package de.osbag.example.service;
+
+
+import de.osbag.example.dto.UserResponse;
+import de.osbag.example.entity.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+
+    public void saveUser(User user) {
+
+        // TODO:
+    }
+
+    public List<UserResponse> allUsers() {
+
+        // TODO: return a list of UserResponse structure with calculated bmi and age.
+        return null;
+    }
+
+}
