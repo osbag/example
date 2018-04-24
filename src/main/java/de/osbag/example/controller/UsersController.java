7862d7a7bee7eb7cce3141c148db85a47a6f807d
okanovic-eldin <okanovic-eldin@users.noreply.github.com>
2018-04-24 20:55:26 +0200

    Add files via upload

--- src/main/java/de/osbag/example/controller/UsersController.java
+++ src/main/java/de/osbag/example/controller/UsersController.java
@@ -1,9 +1,11 @@
 package de.osbag.example.controller;
 
-
 import de.osbag.example.dto.UserResponse;
 import de.osbag.example.entity.User;
 import de.osbag.example.service.UserService;
+import java.net.URI;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.http.HttpStatus;
 
 @RestController
 @RequestMapping("api/v1")
@@ -26,19 +36,29 @@ public class UsersController {
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/users")
-    public ResponseEntity<?> saveUser(@RequestBody User user) {
-
-        userService.saveUser(user);
-
-        return ResponseEntity.created(null).build();
+    public ResponseEntity<?> saveUser(@RequestBody User user) {       
+        if (user != null) {
+            ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+            Validator validator = factory.getValidator();
+            Set<ConstraintViolation<User>> constraints = validator.validate(user);
+            if (constraints.isEmpty()) {
+                userService.saveUser(user);
+                return new ResponseEntity("User created", HttpStatus.CREATED);
+            } else {
+                String bodyHtml = "";
+                for (ConstraintViolation<User> cv : constraints) {
+                    bodyHtml += "Property " + cv.getPropertyPath() + " " + cv.getMessage() + "\n";
+                }
+                return new ResponseEntity<>(bodyHtml, HttpStatus.BAD_REQUEST);
+            }
+        } else {
+            return new ResponseEntity<>("No valid JSON object found in request", HttpStatus.BAD_REQUEST);
+        }
     }
-
 
     @RequestMapping(value = "/users", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-    public ResponseEntity<List<UserResponse>> allUsers() {
-
+    public ResponseEntity<List<UserResponse>> allUsers() {           
         return ResponseEntity.ok().body(userService.allUsers());
     }
-
 
 }
